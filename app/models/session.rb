cefeed4c77962a1b3fd5fc07bbe80c45fdefa6e9
nf2000 <nimraf@chillibean.com>
2021-10-11 17:34:46 +0100

    adding validation more specifc validation for email and password and names lengths

--- app/models/session.rb
+++ app/models/session.rb
@@ -1,2 +1,11 @@
 class Session < ApplicationRecord
+
+    validates :email, :password, presence: true
+    
+    validates :password, length: { in: 6..20 }
+    
+    validates :email, uniqueness: {case_sensitive: false}
+    validates :email, format: /@/
+    
+
 end
