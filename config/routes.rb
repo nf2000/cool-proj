f74d4c292baf2c265b9317ec60108acae365d197
nf2000 <nimraf@chillibean.com>
2021-10-11 07:59:44 +0100

    added a route for logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :sessions, :users
+  resources :sessions, :users, :homes
 
-  #root "session#index"
+  root "homes#index"
 
   get "signup", to: "users#new"
   get "login", to: "sessions#new"
