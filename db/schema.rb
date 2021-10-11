22b0ebd6630300e3c86c8c7f6387abd0fc69ef35
nf2000 <nimraf@chillibean.com>
2021-10-11 08:15:32 +0100

    added fistname surname and removed username from db

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_07_142823) do
+ActiveRecord::Schema.define(version: 2021_10_11_071327) do
 
   create_table "users", force: :cascade do |t|
-    t.string "username"
     t.string "email"
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "firstname"
+    t.string "surname"
   end
 
 end
