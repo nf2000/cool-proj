b0a4b9bc116298ca1167186a891936576756eaf6
nf2000 <nimraf@chillibean.com>
2021-10-11 10:45:28 +0100

    updated destroy action so that it doesn't remove the user from the db

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,21 +4,19 @@ class SessionsController < ApplicationController
         @user = User.find_by_email( params[:session][:email].downcase)
         if @user && @user.authenticate(params[:session][:password])
             session[:user_id] = @user.id
-            flash[:success] = "Logged in successfully."
+            flash.now[:success] = "Logged in successfully."
             return redirect_to root_path
           
         else
            
           flash[:danger] = "There was something wrong with your login details."
-
           render 'new'
         end
       end
        
     def destroy
-        User.find(session[:user_id]).destroy
         session[:user_id] = nil
-        flash[:success] = "You have been logged out."
+        flash.now[:success] = "You have been logged out."
         redirect_to login_path
     end
 
