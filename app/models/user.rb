403fa84a872c400e4196cc7bcdbeaa4d171eb1c0
nf2000 <nimraf@chillibean.com>
2021-10-11 15:14:44 +0100

    adding validation for the signup page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,16 @@
 class User < ApplicationRecord
+    validates :firstname, :surname, :email, :password, :password_confirmation, presence: true
+    validates :password, confirmation: true
+
+    validates :firstname, length: { minimum: 2 }
+    validates :password, length: { in: 6..20 }
+
+    validates :email, uniqueness: { message: " already has an account with it" }
+    validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+
+
+
+
+
     has_secure_password
 end
