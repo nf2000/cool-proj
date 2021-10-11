9eb7d5580b0b4f47cb8567bc99e31bd0f4c9a55b
nf2000 <nimraf@chillibean.com>
2021-10-11 15:15:11 +0100

    schema updated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_11_071327) do
+ActiveRecord::Schema.define(version: 2021_10_11_102336) do
+
+  create_table "sessions", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
