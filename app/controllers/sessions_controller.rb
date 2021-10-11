91aa14f81e9812001972a8e67b4663117594eda9
nf2000 <nimraf@chillibean.com>
2021-10-11 17:33:59 +0100

    updated error messgae for log in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,18 @@
 class SessionsController < ApplicationController
 
+    def new
+    end
+    
     def create
         @user = User.find_by_email( params[:session][:email].downcase)
         if @user && @user.authenticate(params[:session][:password])
-            session[:user_id] = @user.id
+            @session[:user_id] = @user.id
             flash.now[:success] = "Logged in successfully."
             return redirect_to root_path
           
         else
            
-          flash[:danger] = "There was something wrong with your login details."
+          flash[:danger] = "Invalid password or/and email."
           render 'new'
         end
       end
