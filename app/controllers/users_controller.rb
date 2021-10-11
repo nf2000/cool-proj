d221c8e509cf99cd11c29f4f597c0b448fd35526
nf2000 <nimraf@chillibean.com>
2021-10-11 15:18:49 +0100

    space differenct and removed redundant brackets

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,22 +10,23 @@ class UsersController < ApplicationController
       
     def create
         @user = User.new(user_params)
-        @name = @user.firstname
         if @user.save
             session[:user_id] = @user.id
-            flash[:success] = "User created #{@name}"
- 
-            return redirect_to root_path()
+            flash[:success] = "User created #{@user.firstname}"
+            return redirect_to root_path
 
         else
+          flash.now[:danger] = "Could not sign up"
           render 'new'
+
+
         end
     end
       
     private
       
       def user_params
-        params.require(:user).permit(:firstname, :surname, :email, :password)
+        params.require(:user).permit(:firstname, :surname, :email, :password, :password_confirmation)
       end
     
 end
