ecaf107d88e881be3740c8b28de832418c65727a
nf2000 <nimraf@chillibean.com>
2021-10-13 10:49:50 +0100

    updated flash danger message

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     def create
         @user = User.find_by_email( params[:session][:email].downcase)
         if @user && @user.authenticate(params[:session][:password])
-            @session[:user_id] = @user.id
+            session[:user_id] = @user.id
             flash.now[:success] = "Logged in successfully."
             return redirect_to root_path
           
