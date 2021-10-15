cc31df05ba055964bdf378d2bc281a9a7a3d78c9
nf2000 <nimraf@chillibean.com>
2021-10-15 17:00:40 +0100

    indented inverse of part

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,6 +1,7 @@
 class Booking < ApplicationRecord
-    belongs_to :room, inverse_of: :bookings
-    belongs_to :user, inverse_of: :bookings
+    belongs_to :room #, inverse_of: :bookings
+    belongs_to :user #, inverse_of: :bookings
 
 
 end
+                                                                                                                                                                                                            
