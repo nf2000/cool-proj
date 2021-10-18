c95239bb7e90fb4084adc971fc05f9f48037028c
nf2000 <nimraf@chillibean.com>
2021-10-18 15:05:21 +0100

    putting in presence validation for presence of guests

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,6 +2,7 @@ class Booking < ApplicationRecord
     belongs_to :room #, inverse_of: :bookings
     belongs_to :user #, inverse_of: :bookings
 
+    validates :guests, presence: true
 
 end
                                                                                                                                                                                                             
