ffe20c1dc6a87f4076382d1807de284a823c6377
nf2000 <nimraf@chillibean.com>
2021-10-18 15:09:03 +0100

    have a show and create action

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -3,4 +3,26 @@ class RoomsController < ApplicationController
     def index
         @rooms = Room.all
     end
+
+    def new 
+        @room = Room.new
+    end 
+
+    def show 
+        @room = Room.find(params[:id])
+
+    end 
+
+    def create 
+        @room = Room.new(room_param) 
+        redirect_to new_rooms_booking_path()
+    end
+
+    private
+    def room_param
+        params.require(:room).permit(:room_id)
+
+    end
+
+    
 end
