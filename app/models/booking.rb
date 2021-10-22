bbc6041fda55e0c183213ed2e73f9840d1fa1292
nf2000 <nimraf@chillibean.com>
2021-10-22 10:42:36 +0100

    adding a duration to the scope - doesn't work

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -4,9 +4,10 @@ class Booking < ApplicationRecord
 
     validates :guests,:check_in,:check_out, presence: true
     validate :out_date_after_in_date
-    validates_uniqueness_of :room_id, scope: [:check_in]
-    validates_uniqueness_of :room_id, scope: [:check_out]
 
+    duration = [:check_in..:check_out]
+    validates_uniqueness_of :room_id, scope: (duration)
+    #validates_uniqueness_of :room_id, scope: [:check_out]
 
     def out_date_after_in_date
         return if check_in.blank? || check_out.blank?
