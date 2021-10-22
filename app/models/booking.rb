24de7e02f55cb7a76dd7a8abbbc24c266b8269f7
nf2000 <nimraf@chillibean.com>
2021-10-22 16:57:36 +0100

    gem overlaps validation works

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -4,18 +4,13 @@ class Booking < ApplicationRecord
 
     validates :guests,:check_in,:check_out, presence: true
     validate :out_date_after_in_date
-
-    duration = [:check_in..:check_out]
-    validates_uniqueness_of :room_id, scope: (duration)
-    #validates_uniqueness_of :room_id, scope: [:check_out]
+    validates :check_in, :check_out, overlap: {:scope => "room_id", :message_title => "Availablity:", :message_content => "Room is not available"}
 
     def out_date_after_in_date
         return if check_in.blank? || check_out.blank?
-    
         if check_out < check_in
           errors.add(:check_out, "must be after the check In date")
         end
      end
-
 end
                                                                                                                                                                                                             
