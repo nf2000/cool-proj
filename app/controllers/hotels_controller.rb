180c6ce3a6c0a4c459206eb17c245941407590f8
nf2000 <nimraf@chillibean.com>
2021-10-26 17:05:19 +0100

    deleted unrequired hotel controller and model

--- app/controllers/hotels_controller.rb
+++
@@ -1,2 +0,0 @@
-class HotelsController < ApplicationController
-end
--- app/models/hotel.rb
+++
@@ -1,5 +0,0 @@
-class Hotel < ApplicationRecord
-    #has_many :rooms, dependent: :destroy
-    #has_many :bookings, through: :rooms
-
-end
