3a7f16da0a34e1ce2a835277b967ebb4b8f71961
nf2000 <nimraf@chillibean.com>
2021-10-26 17:04:47 +0100

    got rid of extra space

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -3,21 +3,8 @@ class RoomsController < ApplicationController
     def index
         @rooms = Room.all
         @bookings = Booking.all
-        # @rooms = Booking.where.not("check_in <= ? AND check_out >= ?", check_out, check_in)
-        #@booking = Booking.find(params[:check_in, :check_out])
-        @roomsTakenIds = Room.includes(:bookings).where( "bookings.check_in = ? AND bookings.check_out = ?",params[:check_in], params[:check_out] ).pluck(:id)
-
-
-        #@roomsTakenIds = Booking.where.not("check_in = ? ", :check_in).pluck(:room_id)   
+        @roomsTakenIds = Room.includes(:bookings).where( "bookings.check_in = ? AND bookings.check_out = ?",params[:check_in], params[:check_out] ).pluck(:room_id)
         @roomsAvailable = @rooms.filter{|room| !@roomsTakenIds.include?(room.id)}
-
-       
-       #@roomsAvailable = Room.joins(' FULL OUTER JOIN "bookings" ON "rooms"."id" = "bookings"."id" ').where.not("bookings.check_in <= ? AND bookings.check_out >= ?", :check_out, :check_in)
-        Rails.logger.warn("***********")
-        Rails.logger.warn(@roomsTakenIds)
-        Rails.logger.warn("***********" )
-
-        
     end
 
     def new 
