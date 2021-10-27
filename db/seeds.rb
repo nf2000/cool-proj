f78b94ab8167d1d1d86185df2ce84d0f1587f281
nf2000 <nimraf@chillibean.com>
2021-10-27 14:38:29 +0100

    fixing format

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,8 +10,6 @@ d2 =  Date.new(2021,10,24)
 d3 = Date.new(2022,01,01)
 d4 = Date.new(2022,01,02)
 
-
-
 u1 = User.create!(firstname: "Fatima", surname: "Nim", email: "nf@email.com", password: "Password", password_confirmation: "Password")
 
 r1 = Room.create(name: 'Room 1', capacity: 2, cost: 200, bed_no: 1, bed_type: 'King')
@@ -20,5 +18,5 @@ r3 = Room.create(name: 'Room 3', capacity: 1, cost: 100, bed_no: 1, bed_type: 's
 r4 = Room.create(name: 'Room 4', capacity: 5, cost: 1000, bed_no: 3, bed_type: 'Double')
 
 
-b1 = Booking.create(guests: 1, check_in: d3, check_out: d4, user_id: 1, room_id: 2, duration: nil )
+b1 = Booking.create(guests: 1, check_in: d3, check_out: d4, user_id: 1, room_id: 2)
 
