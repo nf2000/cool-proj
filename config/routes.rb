ee27acd6f1e69a084c5c4b7ff7f021b2f3ca7f6f
nf2000 <nimraf@chillibean.com>
2021-10-28 15:07:29 +0100

    spacing difference

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,5 @@ Rails.application.routes.draw do
   get "login", to: "sessions#new"
   post "login", to: "sessions#create"
   delete "logout", to: "sessions#destroy"
-
+  #get "/rooms/:id/bookings", to: 'bookings#index', as: 'bookings'
 end
