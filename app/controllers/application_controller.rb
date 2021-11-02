84064215f9c4694473e081328544366f4d49bcd9
nf2000 <nimraf@chillibean.com>
2021-11-02 11:52:38 +0000

    drying uo code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,17 +5,15 @@ class ApplicationController < ActionController::Base
 
     def current_user
         @current_user ||= User.find_by_id(session[:user_id])
-
     end
     
     def logged_in?
-        current_user != nil
+        !!current_user 
     end
     
     def require_user
-        if !logged_in?
-            flash[:danger] = "You must be logged in to perform that action."
-            redirect_to login_path
-        end
+        return if logged_in?
+        flash[:danger] = "You must be logged in to perform that action."
+        redirect_to login_path
     end
 end
