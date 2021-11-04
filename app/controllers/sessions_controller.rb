9753e5cd26fe9fce9fcba4f761f1614f7d35a45c
nf2000 <nimraf@chillibean.com>
2021-11-04 09:54:17 +0000

    removing white space

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,10 +8,8 @@ class SessionsController < ApplicationController
         if @user && @user.authenticate(params[:session][:password])
             session[:user_id] = @user.id
             flash.now[:success] = "Logged in successfully."
-            return redirect_to root_path
-          
+            redirect_to root_path
         else
-           
           flash[:danger] = "Invalid password or/and email."
           render 'new'
         end
@@ -21,14 +19,5 @@ class SessionsController < ApplicationController
         session[:user_id] = nil
         flash.now[:success] = "You have been logged out."
         redirect_to login_path
-    end
-
-    def edit 
-        if logged_in?
-            return redirect_to logout_path
-          else
-            render  redirect_to login_path
-        end
-        
     end
 end
